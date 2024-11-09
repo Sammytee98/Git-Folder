01de4762d15b807e5bdfc94dc8e072d5e538418f
Anonymous <stosin525@gmail.com>
2024-11-09 21:03:57 +0100

    Modify Hotel Message and Rooms left

--- git.js
+++ git.js
@@ -1,2 +1,9 @@
-const greetMessage = name => `Hello, ${name}! Welcome the Sunrise Hotel.`;
-greetMessage`Samuel`;
+const info = {
+    name: "Samuel",
+    age: 30,
+    totalRooms: 120,
+    bookedRooms: 40,
+    availableRooms: () => this.totalRooms - this.bookedRooms
+}
+const {name, availableRooms} = info;
+const greetingMessage = () => `Hello, ${name}! Welcome to Sunrise Hotel. We have ${availableRooms} unbooked rooms left. Pick your choice!`;
